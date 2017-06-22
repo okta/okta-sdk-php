68b81d9cce3944ba0c44c614aa0f694495489e72
Brian Retterer <bretterer@gmail.com>
2017-06-22 13:15:41 -0400

    Adds okta yaml file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 require __DIR__ . '/BaseTestCase.php';
+
+if (!is_dir(posix_getpwuid(posix_getuid())['dir'] . '/.okta')) {
+    mkdir(posix_getpwuid(posix_getuid())['dir'] . '/.okta');
+}
+
+file_put_contents(
+    posix_getpwuid(posix_getuid())['dir'] . '/.okta/okta.yaml',
+    '{okta: { client: { token:"defaultToken", orgUrl:"https://default-org.okta.com"}}}');
