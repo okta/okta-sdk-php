4fb25237d8c13e8ed6ab032cb08954abaebb4dc5
Brian Retterer <bretterer@gmail.com>
2017-06-27 10:28:22 -0400

    Update script

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -5,4 +5,5 @@ git checkout ${TRAVIS_TAG}
 php ../sami.phar update sami.php
 git checkout ${TRAVIS_TAG}
 mkdir build/gh-pages
-mv build/docs build/gh-pages
+mv build/docs build/gh-pages/
+ls -lah build/gh-pages/
