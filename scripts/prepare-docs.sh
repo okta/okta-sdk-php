570ed2d5b7329aa64061286efccafa34d67aa3f6
Brian Retterer <bretterer@gmail.com>
2017-06-27 11:00:08 -0400

    Adds Redirector

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -4,5 +4,6 @@ cd full
 git checkout ${TRAVIS_TAG}
 php ../sami.phar update sami.php
 git checkout ${TRAVIS_TAG}
+mkdir build/gh-pages/latest
 php scripts/create-redirector.php build/gh-pages/latest/index.html
 php scripts/create-redirector.php build/gh-pages/index.html
