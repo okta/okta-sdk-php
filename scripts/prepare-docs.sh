96e2a09409b758f71566ef92248df2aeee55d51d
Brian Retterer <bretterer@gmail.com>
2017-06-27 09:40:36 -0400

    Testing Sami

--- scripts/prepare-docs.sh
+++ scripts/prepare-docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-php sami.phar update sami.php
+curl -I http://get.sensiolabs.org/sami.phar
+git clone https://${{GH_REPO}} full
+cd full
+git checkout ${TRAVIS_TAG}
+php ../sami.phar update sami.php
 git checkout ${TRAVIS_TAG}
 mkdir build/gh-pages
 mv build/docs /build/gh-pages
