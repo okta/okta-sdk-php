0c327107206ffa02a76139ec185fcc8c8b229784
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-04-30 10:26:45 -0700

    Runs integration tests only for cron job

--- scripts/cron-test.sh
+++ scripts/cron-test.sh
@@ -2,4 +2,8 @@ export OKTA_CLIENT_ORGURL=$TREX_OKTA_CLIENT_ORGURL
 export OKTA_CLIENT_TOKEN=$TREX_OKTA_CLIENT_TOKEN
 export OKTA_MOCK_TESTS=false
 
+echo '------------------'
+echo $OKTA_CLIENT_ORGURL
+echo '------------------'
+
 composer test:integration
