ce40f2a2276de7e633284153b6d0900da6e52281
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2018-08-21 14:53:13 -0700

    Update cron job to run tests for 7.2

--- scripts/cron-test.sh
+++ scripts/cron-test.sh
@@ -2,8 +2,8 @@ export OKTA_CLIENT_ORGURL=$TREX_OKTA_CLIENT_ORGURL
 export OKTA_CLIENT_TOKEN=$TREX_OKTA_CLIENT_TOKEN
 export OKTA_MOCK_TESTS=false
 
-# If its not php version 7.1, exit
-php --version | grep -w 7.1
+# Run tests for php 7.2. If its not 7.2, exit
+php --version | grep -w 7.2
 
 if [[ $? != 0 ]] ; then
     exit 0
