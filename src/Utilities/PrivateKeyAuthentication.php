fe9cf1b66269b333cd55066fb847152e04139900
Brian Retterer <bretterer@gmail.com>
2020-03-02 15:09:35 -0500

    debug

--- src/Utilities/PrivateKeyAuthentication.php
+++ src/Utilities/PrivateKeyAuthentication.php
@@ -124,7 +124,7 @@ class PrivateKeyAuthentication {
         $info = curl_getinfo($curl);
         $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         if ($httpcode < 200 || $httpcode > 299) {
-            
+            var_dump($token, $this->orgUrl, $query);
             $error = new Error(json_encode($token));
             throw new ResourceException($error);
         }
