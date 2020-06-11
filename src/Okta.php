0ddc1a290afb303ee8de00e89c72fe87778a3e92
Brian Retterer <bretterer@gmail.com>
2020-06-11 16:43:34 -0400

    Luke towers patch 1 (#87)
    Co-authored-by: Luke Towers <github@luketowers.ca>

--- src/Okta.php
+++ src/Okta.php
@@ -28,7 +28,7 @@ use Okta\Users\User;
 class Okta
 {
 
-    const VERSION = '1.2.0';
+    const VERSION = '1.2.1';
 
     public function __construct(Client $client = null, DefaultDataStore $dataStore = null)
     {
